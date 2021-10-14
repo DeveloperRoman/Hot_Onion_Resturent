a7b94048b54c3e67c4b25f629237deb54238a7b3
roman <romansheikh170@gmail.com>
2021-10-15 00:09:02 +0600

    --- Auto-Git Commit ---

--- src/Pages/Home/Main/SeeAllButton/SeeAllButton.js
+++ src/Pages/Home/Main/SeeAllButton/SeeAllButton.js
@@ -1,18 +1,10 @@
 import React from 'react';
 import './SeeAllButton.css';
 
-
-const checkoutFood = {
-    text-transform: capitalize;
-    background: #999999a6;
-    color: white;
-    font-weight: 700;
-}
-
 const SeeAllButton = () => {
     return (
         <div className='text-center'>
-            <button className="btn checkout-food">checkout all our food</button>
+            <button className="btn text-white checkout-food">checkout all our food</button>
         </div>
     );
 };
