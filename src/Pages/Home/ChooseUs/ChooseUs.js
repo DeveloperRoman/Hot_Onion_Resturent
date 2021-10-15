406c17ce4d94fca82c2a2059e20aac6829c06ac0
roman <romansheikh170@gmail.com>
2021-10-15 09:00:42 +0600

    --- Auto-Git Commit ---

--- src/Pages/Home/ChooseUs/ChooseUs.js
+++ src/Pages/Home/ChooseUs/ChooseUs.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import { Container, Row } from 'react-bootstrap';
 import Services from './Services';
 
 const ChooseUs = () => {
     return (
         <div>
-            <Services></Services>
+            <Container>
+                <Row>
+                    <Services></Services>
+                </Row>
+            </Container>
         </div>
     );
 };
--- src/Pages/Home/ChooseUs/Services.js
+++ src/Pages/Home/ChooseUs/Services.js
@@ -1,11 +1,35 @@
+import { faArrowRight, faGift } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
+import { Col } from 'react-bootstrap';
 import fastDelevery from '../../../images/services/fast.png'
 
 const Services = () => {
     return (
-        <div>
-            this is services
-        </div>
+        <Col md={4}>
+            <div>
+                <div>
+                    <img className="img-fluid" src={fastDelevery} alt="" />
+                </div>
+                <div className="d-flex p-3 align-items-start">
+                    <div className="me-3">
+                        <div className="bg-danger p-2 rounded-circle text-white d-flex align-items-center justify-content-center">
+                        <FontAwesomeIcon icon={faGift}></FontAwesomeIcon>
+                        </div>
+                    </div>
+                    <div>
+                        <h4>Fast Delevery</h4>
+                        <p style={{fontSize: '14px'}} className="text-muted">Keep your systems in sync with automated web hook based notifications each time link is paid and how we dream about our future.</p>
+                        <button className="border-0 text-primary bg-transparent fw-bold">
+                            See More
+                            <span className="ms-3 bg-success rounded-circle text-white p-1" style={{fontSize: '12px'}}>
+                                <FontAwesomeIcon icon={faArrowRight}></FontAwesomeIcon>
+                            </span>
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </Col>
     );
 };
 
