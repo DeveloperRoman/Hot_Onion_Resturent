cb57538d3750e56724c665e862f8ab6d62cc936e
roman <romansheikh170@gmail.com>
2021-10-15 14:27:07 +0600

    --- Auto-Git Commit ---

--- src/Pages/Home/ChooseUs/ChooseUs.js
+++ src/Pages/Home/ChooseUs/ChooseUs.js
@@ -4,17 +4,30 @@ import Services from './Services';
 import fastDelevery from '../../../images/services/fast.png'
 import cook from '../../../images/services/cook.png'
 import homeDelevery from '../../../images/services/home.png'
+import { faBell, faBusAlt, faTruck } from '@fortawesome/free-solid-svg-icons';
 
 const ChooseUs = () => {
     return (
-        <div>
-            <Container>
-                <h2>Why you choose us</h2>
+        <div className="mt-5">
+            <Container fluid="sm">
+                <h2 className="mt-4">Why you choose us</h2>
                 <p>Barton waited twenty always repair in within we do. An delighted offending <br /> curiosity my is dashwoods at. Boy prospous increasing surrounded.</p>
                 <Row>
-                    <Services img={fastDelevery} title={''}></Services>
-                    <Services></Services>
-                    <Services></Services>
+                    <Services
+                        img={fastDelevery}
+                        title={'Fast Delevery'}
+                        icon={faBusAlt}>
+                    </Services>
+                    <Services
+                        img={cook}
+                        title={'A Good Auto Responder'}
+                        icon={faBell}>
+                    </Services>
+                    <Services
+                        img={homeDelevery}
+                        title={'Home Delivery'}
+                        icon={faTruck}>
+                    </Services>
                 </Row>
             </Container>
         </div>
--- src/Pages/Home/ChooseUs/Services.js
+++ src/Pages/Home/ChooseUs/Services.js
@@ -1,24 +1,23 @@
-import { faArrowRight, faGift } from '@fortawesome/free-solid-svg-icons';
+import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 import { Col } from 'react-bootstrap';
-import fastDelevery from '../../../images/services/fast.png'
 
-const Services = () => {
+const Services = (props) => {
     return (
-        <Col md={4}>
+        <Col sm={6} md={4}>
             <div>
                 <div>
-                    <img className="img-fluid" src={fastDelevery} alt="" />
+                    <img className="img-fluid" src={props.img} alt="" />
                 </div>
                 <div className="d-flex p-3 align-items-start">
                     <div className="me-3">
                         <div className="bg-danger p-2 rounded-circle text-white d-flex align-items-center justify-content-center">
-                        <FontAwesomeIcon icon={faGift}></FontAwesomeIcon>
+                        <FontAwesomeIcon icon={props.icon}></FontAwesomeIcon>
                         </div>
                     </div>
                     <div>
-                        <h4>Fast Delevery</h4>
+                        <h4>{props.title}</h4>
                         <p style={{fontSize: '14px'}} className="text-muted">Keep your systems in sync with automated web hook based notifications each time link is paid and how we dream about our future.</p>
                         <button className="border-0 text-primary bg-transparent fw-bold">
                             See More
